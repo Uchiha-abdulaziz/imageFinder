73cacaf9ec8d7da28ff9aadb28368d34d6faecb3
Abdulaziz <Abdulaziz>
2023-03-27 17:09:19 +0600

    style changes

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ export default class App extends Component {
     images: [],
     query: "",
     page: 1,
-    per_page: 20,
+    per_page: 9,
     isLoading: false,
   };
 
   fatchImages = async () => {
-    const { query } = this.state;
+    const { query, per_page } = this.state;
     try {
       await axios
         .get(
-          `https://pixabay.com/api/?key=34695797-1fae3d9441fb187335514c8af&q=${query}&image_type=photo`
+          `https://pixabay.com/api/?key=34695797-1fae3d9441fb187335514c8af&q=${query}&image_type=photo&per_page=${per_page}`
         )
         .then((response) => {
           const data = response.data.hits;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+/* import React, { Component } from "react"; 
+ 
+export default class Searchbar extends Component { 
+  state = { 
+    query: "", 
+  }; 
+ 
+  handleChange = (event) => { 
+    const { query, value } = event.currentTarget; 
+    this.setState({ 
+      query: value, 
+    }); 
+  }; 
+  handleSubmit = (event) => { 
+    const { props, state } = this; 
+    event.preventDefault(); 
+    props.onSubmit(state.query); 
+    this.setState({ query: "" }); 
+  }; 
+  render() { 
+    return ( 
+      <> 
+        <header className="Searchbar"> 
+          <form className="SearchForm" onSubmit={this.handleSubmit}> 
+            <button type="submit" className="SearchForm-button"> 
+              <span className="button-label"></span> 
+            </button> 
+ 
+            <input 
+              className="SearchForm-input" 
+              type="text" 
+              autoComplete="off" 
+              autoFocus 
+              placeholder="Search images and photos" 
+              value={this.state.query} 
+              onChange={this.handleChange} 
+            /> 
+          </form> 
+        </header> 
+      </> 
+    ); 
+  } 
+} 
+ */
+import React, { Component } from "react";
 
-function Searchbar() {
-  return <div>Searchbar</div>;
+export default class Searchbar extends Component {
+  state = {
+    query: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({ query: event.currentTarget.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: "" });
+  };
+  render() {
+    return (
+      <>
+        {" "}
+        <header className="Searchbar">
+          <form className="SearchForm" onSubmit={this.handleSubmit}>
+            <button type="submit" className="SearchForm-button">
+              <span className="button-label"></span>
+            </button>
+
+            <input
+              className="SearchForm-input"
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              value={this.state.query}
+              onChange={this.handleChange}
+            />
+          </form>
+        </header>
+      </>
+    );
+  }
 }
-
-export default Searchbar;
